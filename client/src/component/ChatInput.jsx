85caafadc40611bc052b326b351a92fcecebc9df
Pankajpatel19 <pankajpatelinter@gmail.com>
2026-06-22 13:22:48 +0530

    Refactor : update chatInput component

--- client/src/component/ChatInput.jsx
+++ client/src/component/ChatInput.jsx
@@ -3,7 +3,8 @@ import { ChatContext } from "../context/ChatContextProvider";
 import axios from "axios";
 import { toast } from "react-toastify";
 function ChatInput() {
-  const { text, setText, setResponse, isLoading, setIsLoading } = useContext(ChatContext);
+  const { text, setText, setResponse, isLoading, setIsLoading } =
+    useContext(ChatContext);
 
   const [resume, setResume] = useState(null);
 
@@ -51,12 +52,14 @@ function ChatInput() {
           <input
             className="w-full h-10 p-3 rounded-l-lg focus:outline-none bg-transparent disabled:opacity-50"
             type="text"
-            name="prompt"
-            id="prompt"
-            placeholder={isLoading ? "Processing..." : "Prompt"}
+            name="job_description"
+            id="job_description"
+            placeholder={isLoading ? "Processing..." : "Job Description"}
             value={text}
             onChange={(e) => setText(e.target.value)}
-            onKeyDown={(e) => e.key === "Enter" && !isLoading && handleSubmit(resume)}
+            onKeyDown={(e) =>
+              e.key === "Enter" && !isLoading && handleSubmit(resume)
+            }
             disabled={isLoading}
           />
           <input
@@ -88,9 +91,25 @@ function ChatInput() {
           disabled={text.trim() === "" || !resume || isLoading}
         >
           {isLoading ? (
-            <svg className="animate-spin h-5 w-5 text-neutral-800" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-              <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
-              <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+            <svg
+              className="animate-spin h-5 w-5 text-neutral-800"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <circle
+                className="opacity-25"
+                cx="12"
+                cy="12"
+                r="10"
+                stroke="currentColor"
+                strokeWidth="4"
+              ></circle>
+              <path
+                className="opacity-75"
+                fill="currentColor"
+                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+              ></path>
             </svg>
           ) : (
             "→"
